80b0453978f39d9d8a0a6b80f6c88428223d3888
Cole Winchester <cowinch@gmail.com>
2023-09-18 19:27:18 -0500

    info box complete

--- script.js
+++ script.js
@@ -2,27 +2,29 @@ let cursor = document.querySelector("#cursor")
 let cursorSize = document.documentElement.style.setProperty
 
 document.addEventListener('mousemove', (e, interacting) => {
-    cursor.style.top = e.pageY + 'px'
-    cursor.style.left = e.pageX + 'px'
+    cursor.style.top = e.pageY+5 + 'px'
+    cursor.style.left = e.pageX+5 + 'px'
 })
 
 window.onmousemove = e => {
     const interactable = e.target.closest(".interactable"),
     interacting = interactable !== null;
     if(interacting){
-        document.documentElement.style.setProperty('--cursorSize', '125px')
+        document.documentElement.style.setProperty('--cursorSize', '180px')
+        document.documentElement.style.setProperty('--cursorOpacity', '1')
     } else {
         document.documentElement.style.setProperty('--cursorSize', '0px')
+        document.documentElement.style.setProperty('--cursorOpacity', '0')
     }
 
     if(e.target.closest('#himalayas')){
-        cursor.innerHTML = "The Himalayas lorem ipsum blah blah blah blah blah"
+        cursor.innerHTML = document.getElementById('himalayas-text').innerHTML
     }
     else if (e.target.closest('#zhangjiajie')){
-        cursor.innerHTML = "Zhangjiajie"
+        cursor.innerHTML = document.getElementById('zhangjiajie-text').innerHTML
     }
     else if (e.target.closest('#fuji')){
-        cursor.innerHTML = "Mount Fuji"
+        cursor.innerHTML = document.getElementById('fuji-text').innerHTML
     }
     else {
         cursor.innerHTML = null
